15bf77fcdcbca2223ddfce1567d127237f06f849
brenin <130005400+bren112@users.noreply.github.com>
2024-06-16 21:16:23 -0300

    Update Home.jsx

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,8 +1,9 @@
-import React from 'react'
-
+import React from 'react';
+import img from './img.png';
 const Home = () => {
   return (
-    <div>CONTROLE SEU EVENTO AQUI :)</div>
+    <div>CONTROLE SEU EVENTO COM A FESTFY!</div>
+     <img src={img} alt="" srcset="" />
   )
 }
 
